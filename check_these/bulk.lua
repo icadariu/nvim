f3f7777d9cdf99256054784886c80ef77bd3d429
Ionut Cadariu <ionut.cadariu@gmail.com>
2024-10-11 01:01:59 +0200

    Refactor (#5)
    * chore: refactor
    
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>
    
    * chore: refactor
    
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>
    
    * feat: revisit options/remaps/globals
    
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>
    
    * fix: conflicts
    
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>
    
    * refactor: small fix
    
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>
    
    ---------
    
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>

---
+++ check_these/bulk.lua
@@ -0,0 +1,14 @@
+--- Bulk plugins to check
+
+return {
+  -- From vmarlier
+  { 'echasnovski/mini.cursorword', version = false }, -- highlight word under cursor in normal mode
+  { 'echasnovski/mini.trailspace', version = '*' },   -- trim space or whiteline easily
+  'rcarriga/nvim-notify',                             -- better notifications
+  -- UI Enhancements
+  'folke/which-key.nvim',                             -- key mappings configuration
+  'nvim-tree/nvim-web-devicons',                      -- devicons
+  'rcarriga/nvim-notify',                             -- better notifications
+  'akinsho/bufferline.nvim',                          -- configurable bufferline
+  'nvim-lualine/lualine.nvim',                        -- configurable statusline
+}
--- lua/remap.lua
+++ lua/remap.lua
@@ -1,75 +1,115 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Execute stuff
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clears the search" })
+vim.keymap.set("n", "<leader>gff", ":!gofmt -w %<CR><CR>", { desc = "Run go fmt" })
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true, desc = "Save file" })
+vim.keymap.set("n", "<leader>Cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make file executable" })
 
---vim.keymap.set("n", "<leader>,f", ":StripWhitespace<CR>") -- Remove whitespaces
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")        -- This keymap clears the search
-vim.keymap.set("n", "<leader>gff", ":!gofmt -w %<CR><CR>") -- Go fmt
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true }) -- save file in normal mode
+-- Exit insert mode without Escape
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", { desc = "Exit from insert mode", noremap = true })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
+-- Select all
+vim.keymap.set("n", "==", "ggVG", { desc = "Select the entire file" })
+
+-- Split navigation
+--  Use CTRL+<hjkl> to switch between windows. Check `:help wincmd` for more.
 vim.keymap.set("n", "<C-j>", "<C-W><C-j>", { desc = "Move focus down" })
 vim.keymap.set("n", "<C-k>", "<C-W><C-k>", { desc = "Move focus up" })
 vim.keymap.set("n", "<C-h>", "<C-W><C-h>", { desc = "Move focus left" })
 vim.keymap.set("n", "<C-l>", "<C-W><C-l>", { desc = "Move focus right" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")              -- Move selected lines down
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")              -- Move selected lines up
+-- Keep window centered when navigate up down
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor at same position" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page ; keep the current line in center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page ; keep the current line in center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next match; center the line; match is highlighted" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous match; center the line; match is highlighted" })
 
-vim.keymap.set("n", "J", "mzJ`z")                         -- Join the line below with the current one and keep cursor at current position.
-vim.keymap.set("n", "<C-d>", "<C-d>zz")                   -- Scroll down half page ; keep the current line in center
-vim.keymap.set("n", "<C-u>", "<C-u>zz")                   -- Scroll up half page ; keep the current line in center
-vim.keymap.set("n", "n", "nzzzv")                         -- Next match; center the line; match is highlighted
-vim.keymap.set("n", "N", "Nzzzv")                         -- Previous match; center the line; match is highlighted
+-- Paste without overwriting register
+vim.keymap.set("v", "p", '"_dP')
 
-vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>") -- Restart LSP
-
--- "cut" the visually selected text and paste it elsewhere, similar to the traditional cut-and-paste
-vim.keymap.set("x", "<leader>p", [["_dP]]) -- Pastes in visual mode, replacing the selection, but without overwriting the default register
-
--- Copies selected text(or current line) into the system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- Yank to system clipboard in normal/visual mode
-vim.keymap.set("n", "<leader>Y", [["+Y]])          -- Yank the current line to system clipboard in normal mode
+-- Copy selected text(or current line) into the system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard in n/v mode" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank the current line to system clipboard in normal mode" })
 
 -- useful when you want to remove text entirely without cluttering your yank/delete history
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- Exit insert mode using Ctrl+c or jj
-vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true })
+-- Disable Q, it usually trigger :Ex
+vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "Q", "<nop>")                    -- Disable Q in normal mode since Q usually trigger :Ex
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- Trigger formatting providd by the LSP
+-- Buffer Close
+vim.keymap.set("n", "<leader>q", "<cmd>bd!<CR>", { desc = "Force Close Buffer" })
+vim.keymap.set("n", "<leader>w", "<cmd>bp|bd #<CR>", { desc = "Close Buffer; Retain Split" })
 
-vim.keymap.set("n", "<C-M-k>", "<cmd>cnext<CR>zz")     -- Jump to the next quick fix using Ctrl + Alt + J
-vim.keymap.set("n", "<C-M-j>", "<cmd>cprev<CR>zz")     -- Jump to the next quick fix using Ctrl + Alt + J
+-- Navigate between quickfix items
+vim.keymap.set("n", "<C-M-k>", "<cmd>cnext<CR>zz", { desc = "Forward qfixlist" }) -- Jump to the next quick fix using Ctrl + Alt + J
+vim.keymap.set("n", "<C-M-j>", "<cmd>cprev<CR>zz", { desc = "Backward qfixlist" })
 
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz") -- Navigate to the next LSP diagnostic
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- Navigate between location list items
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Forward location list" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Backward location list" })
 
---[[
-This keymap finds all occurrences of the word under your cursor and replaces each occurrence with itself,
-but inserted at the beginning of its line. This can be useful for tasks like rearranging code elements
-or quickly creating lists from scattered text.
---]]
---vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>Cx", "<cmd>!chmod +x %<CR>", { silent = true }) -- Make current file executable
+-- Replace word under cursor across entire buffer
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Replace word under cursor" })
+
+
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Trigger formatting providd by the LSP" })
 
 -- Creates a basic Go error handling structure, allowing you to quickly fill in the specific error handling logic within the block.
 vim.keymap.set(
   "n",
   "<leader>ee",
-  "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+  "oif err != nil {<CR>}<Esc>Oreturn err<Esc>",
+  { desc = "Create basic Go error handling structure" }
 )
 
--- Function to toggle cursorcolumn
-function ToggleCursorColumn()
-  vim.opt.cursorcolumn = not vim.opt.cursorcolumn:get()
-end
+-- Telescope's keymaps search
+vim.api.nvim_set_keymap('n', '<leader>km', ':Telescope keymaps<CR>',
+  { noremap = true, silent = true, desc = "Telescope keymaps" })
 
--- Map a key (for example, <leader>cc) to toggle cursorcolumn
-vim.api.nvim_set_keymap('n', '<leader>cc', ':lua ToggleCursorColumn()<CR>', { noremap = true, silent = true })
+-- Copy file paths
+vim.keymap.set("n", "<leader>cf", "<cmd>let @+ = expand(\"%\")<CR>", { desc = "Copy File Name" })
+vim.keymap.set("n", "<leader>cp", "<cmd>let @+ = expand(\"%:p\")<CR>", { desc = "Copy File Path" })
 
--- Set a keymap to open Telescope's keymaps search
-vim.api.nvim_set_keymap('n', '<leader>km', ':Telescope keymaps<CR>', { noremap = true, silent = true })
+-- Resize with arrows
+vim.keymap.set("n", "<C-S-Down>", ":resize +2<CR>", { desc = "Resize Split Down" })
+vim.keymap.set("n", "<C-S-Up>", ":resize -2<CR>", { desc = "Resize Split Up" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Resize Split Down" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Resize Split Up" })
+
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Move block
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Block Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Block Up" })
+
+-- Search for highlighted text in buffer
+vim.keymap.set("v", "//", 'y/<C-R>"<CR>', { desc = "Search for highlighted text" })
+
+
+-- Autocommands
+vim.api.nvim_create_augroup("custom_buffer", { clear = true })
+
+-- Highlight yanks
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group    = "custom_buffer",
+  pattern  = "*",
+  callback = function() vim.highlight.on_yank { timeout = 200 } end
+})
+
+-- makrdown filetype changes indentation. Fix it usig this
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
