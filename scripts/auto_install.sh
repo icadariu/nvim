9443db68cab4e5335fb77de350587a48226d69f7
Ionut Cadariu <ionut.cadariu@gmail.com>
2024-10-17 23:01:27 +0200

    feat: init
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>

--- scripts/auto_install.sh
+++ scripts/auto_install.sh
@@ -4,6 +4,4 @@ OS=$(lsb_release -si)
 
 [[ ${OS} != "Ubuntu" ]] && echo "This script has been tested on Ubuntu only." && exit 1
 
-echo "It's reco"
-
 sudo apt update && sudo apt install -y git make ripgrep
