a3b200b739082f9ddf6c6f7e1836135fe0c89ff8
Ionut Cadariu <ionut.cadariu@gmail.com>
2024-11-02 21:49:21 +0100

    feat: updates on theme
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>

--- lua/plugins/color_themes.lua
+++ lua/plugins/color_themes.lua
@@ -75,8 +75,10 @@ return {
   },
   {
     "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme "tokyonight-night"
+      vim.cmd.hi "Comment gui=none"
+    end,
   },
 }
