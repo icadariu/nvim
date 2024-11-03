1fc06c9cf48cfd83d6ff7e2c7cf075158454a223
Ionut Cadariu <511477+icadariu@users.noreply.github.com>
2024-11-03 21:52:25 +0100

    feat: comment using ctrl+/
    Signed-off-by: Ionut Cadariu <511477+icadariu@users.noreply.github.com>

--- lua/plugins/comment-nvim.lua
+++ lua/plugins/comment-nvim.lua
@@ -3,5 +3,17 @@ return {
   event = "VeryLazy",
   config = function()
     require("Comment").setup()
+
+    local api = require "Comment.api"
+    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+
+    vim.keymap.set("n", "<C-_>", function()
+      require("Comment.api").toggle.linewise.current()
+    end, { desc = "Toggle comment line (Ctrl+/)", noremap = true, silent = true })
+
+    vim.keymap.set("x", "<C-_>", function()
+      vim.api.nvim_feedkeys(esc, "nx", false)
+      api.toggle.linewise(vim.fn.visualmode())
+    end, { desc = "Toggle comment selection (Ctrl+/)" })
   end,
 }
