b28e52c71917b1498f36ed033649e93be1f8d829
Ionut Cadariu <46490120+icadariu-talend@users.noreply.github.com>
2024-11-05 09:25:40 +0100

    feat: updates on yaml (#2)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -206,24 +206,6 @@ return {
           },
         },
       },
-      yamlls = {
-        capabilities = capabilities,
-        settings = {
-          yaml = {
-            format = { enable = true },
-            schemas = {
-              kubernetes = { "*.yaml", "*.yml" },
-              -- ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-              -- ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-              -- ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
-              -- ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-              -- ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-              -- ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-              -- ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-            },
-          },
-        },
-      },
       lua_ls = {
         settings = {
           Lua = {
